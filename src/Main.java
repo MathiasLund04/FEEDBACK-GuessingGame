48c8e136c47c0ef8b591aebb90befd384893af40
MathiasLund04 <mal034@edu.zealand.dk>
2025-09-18 20:25:03 +0200

    1.1

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         if (menu == 1) {
             playGame();
         } else if (menu==2) {
+            System.out.println("afslutter Programmet");
             System.exit(0);
         } else {
             System.out.println("ugyldigt kommando");
@@ -22,13 +23,13 @@ public class Main {
 
     public static int difficulty(Scanner input) {
 
-        System.out.println("Vælg sværhedsgrad: 1,2,3?");
+        System.out.println("Vælg sværhedsgrad: \n1. let 1-10 \n2. mellem 1-50 \n3. svær 1-100");
         int max;
 
         switch (max = input.nextInt()) {
             case 1: max = 10; break;
-            case 2: max = 30; break;
-            case 3: max = 50; break;
+            case 2: max = 50; break;
+            case 3: max = 100; break;
             default:
                 System.out.println("ugyldig sværhedsgrad prøv igen");
         }
@@ -43,15 +44,19 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int max = difficulty(input);
         int target = randomNumber(max);
+        int tries = 0;
         System.out.println("jeg vælger et tal mellem 0 og " + max);
 
         int guess = 0;
         while (guess != target) {
             System.out.print("indtast dit gæt: ");
             guess = input.nextInt();
+            tries++;
+
             System.out.println(checkGuess(guess, target));
 
         }
+        System.out.println("Tillykke! Du har gættet tallet på " + tries + " forsøg!");
         handlePlayAgain(input);
     }
 
@@ -69,11 +74,12 @@ public class Main {
 
         while (true) {
             System.out.println("vil du spille igen?(ja/nej) ");
-            String answer = input.next().toLowerCase();
+            String answer = input.next().trim().toLowerCase();
 
             switch (answer) {
                 case "ja": playGame(); break;
-                case "nej": System.exit(0);
+                case "nej":
+                    System.out.println("afslutter programmet") ;System.exit(0);
                 default: System.out.println("ugyldigt svar, svar ja eller nej"); break;
             }
         }
